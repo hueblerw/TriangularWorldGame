9b27b23aa43ba19817bd01c4ab87e6ff42ee517f
William Huebler <william.huebler@nordstrom.com>
2020-12-28 09:59:04 -0600

    starting the nation sequence logic

---
+++ Assets/GameSquences/NationCreationSequence.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NationCreationSequence
+{
+    
+	public static Vector2 chooseWhereToPlaceVillage(){
+		string message = "Choose a location for your starting village.  Click Submit once chosen.";
+		Dictionary<string, string> buttons = new Dictionary<string, string>();
+		buttons["Submit"] = "tile_coordinates";
+		InteractableMessageView messageView = new InteractableMessageView(message, buttons);
+		return new Vector2(1, 1);
+	}
+
+	public static string chooseRuralTile(){
+		return null;
+	}
+
+}
